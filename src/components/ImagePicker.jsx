0f43149c6835642c29de316c77d8ca96dbff4791
Suraj Bhola <surajbhola1001@gmail.com>
2024-03-05 22:50:41 +0530

    adding files

--- src/components/ImagePicker.jsx
+++ src/components/ImagePicker.jsx
@@ -10,7 +10,7 @@ export default function ImagePicker({ images, selectedImage, onSelect }) {
             className={selectedImage === image.path ? 'selected' : undefined}
           >
             <img
-              src={`http://localhost:3000/${image.path}`}
+              src={`https://iamsun-event-manager-1.onrender.com/${image.path}`}
               alt={image.caption}
             />
           </li>
